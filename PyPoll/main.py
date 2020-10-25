4a701982a045e8af30b180758cde5d46447427d0
teresa luna <tmluna_18@outlook.com>
2020-10-24 23:56:22 -0500

    submitting script for HW3 PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -10,3 +10,9 @@ mydf = pd.read_csv(mycsv)
 Total_Votes = mydf["Voter ID"].count()
 print(Total_Votes)
 
+
+cand_names = mydf["Candidate"].unique()
+print(cand_names)
+
+votes_percent = cand_names.value_counts()
+print(votes_percent)
