d2dd69c1892cbb17bde7d16f2a75c6f0931ef388
teresa luna <tmluna_18@outlook.com>
2020-10-25 15:38:21 -0500

    Adding final script for HW3 PyBank and PyPoll

--- PyBank/main.py
+++ PyBank/main.py
@@ -13,98 +13,60 @@ Total_Months = mydf["Date"].count()
 #Net total of the Profit/Losses column of dataset
 Total = mydf["Profit/Losses"].sum()
 
-#format the net total to currency without commas or decimals
-Net_Total = '${:}'.format(Total)
-
-#get the mean for the Profit/Losses column of dataset
-    #identify the values in the column
+#identify the values Profit/losses in the column
 Profit_Loss = mydf["Profit/Losses"].values
 
-    #list to hold the values
+#lists to hold the values and monthly changes
 profit = []
-
-    #list to hold the monthly changes 
 monthly_change = []
 
-    #iterate through Profit_Loss to add to the profit list
+#iterate through Profit_Loss and profit to add to lists
 for x in Profit_Loss:  
     profit.append(x)
     
-    #iterate through Profit_Loss to add to the monthly_change list
 for i in range(len(profit)-1):
     monthly_change.append(profit[i+1]-profit[i])
     
-    #get the mean and round
+#get the mean of monthly changes and round
 Avg_change = round(sum(monthly_change)/85,2)
 
-#format the mean to currency without commas and decimals
-Average_Change = '${:}'.format(Avg_change)
-
-#get the greatest increase in profits
+#get the greatest increase and greatest Decrease in profits/losses
 Increase = max(monthly_change)
-
-#format the greatest increase without commas or decimals
-Greatest_Increase = '${:}'.format(Increase)
-
-#need to find the date of the Greatest Increase with a df
-Inc_Date = mydf[mydf["Profit/Losses"]==1170593]
-
-#pull date out of df so only the Date can be printed
-Great_Inc_Date = Inc_Date['Date']
-
-#enter date in quotes so can be printed in output without extra "Name:, dtype"
-Greatest_Inc_Date = "Feb-2012"
-
-#get the greatest decrease in losses
 Decrease = min(monthly_change)
 
-#format the greatest decrease without commas or decimals
-Greatest_Decrease = '${:}'.format(Decrease)
-
-#need to find the date of the Greatest Decrease with a df
+#find the date of the Greatest Increase and Greatest Decrease
+Inc_Date = mydf[mydf["Profit/Losses"]==1170593]
 Dec_Date = mydf[mydf["Profit/Losses"]==-1196225]
 
-#pull date out of df so only the Date can be printed
+#pull dates out of the dfs
+Great_Inc_Date = Inc_Date['Date']
 Great_Dec_Date = Dec_Date["Date"]
 
-#enter date in quotes so can be printed in output without extra "Name:, dtype"
-Greatest_Dec_Date = "Sep-2013"
+#format dates so only dates will print in output
+Greatest_Inc_Date = (Great_Inc_Date).values
+Greatest_Dec_Date = (Great_Dec_Date).values
 
 #Print all the data to the terminal
 print("Financial Analysis")
-
 print("-----------------------------------------")
-
 print("Total Months:", Total_Months)
-
-print("Total:", Net_Total)
-
-print("Average Change:", Average_Change)
-
-print("Greatest Increase in Profits:", Greatest_Inc_Date, Greatest_Increase)
-
-print("Greatest Decrease in Profits:", Greatest_Dec_Date, Greatest_Decrease)
+print("Total:", '${:}'.format(Total))
+print("Average Change:", '${:}'.format(Avg_change))
+print("Greatest Increase in Profits:", str(Greatest_Inc_Date)[1:-1], '${:}'.format(Increase))
+print("Greatest Decrease in Profits:", str(Greatest_Dec_Date)[1:-1], '${:}'.format(Decrease))
 
 #define an output path for a text file
 output_path = "C:\\Users\\tmlun\\python-challenge\\PyBank\\Analysis\\textfile.txt"
-# output_path = "C:\\Users\\tmlun\\python-challenge\\PyBank\\Analysis\\"
-
 txtfile = open(output_path,"w+")
 
 #Print all the data to the txt file
 print("Financial Analysis", file=txtfile)
-
 print("-----------------------------------------", file=txtfile)
-
 print("Total Months:", Total_Months, file=txtfile)
-
-print("Total:", Net_Total, file=txtfile)
-
-print("Average Change:", Average_Change, file=txtfile)
-
-print("Greatest Increase in Profits:", Greatest_Inc_Date, Greatest_Increase, file=txtfile)
-
-print("Greatest Decrease in Profits:", Greatest_Dec_Date, Greatest_Decrease, file=txtfile)
+print("Total:", '${:}'.format(Total), file=txtfile)
+print("Average Change:", '${:}'.format(Avg_change), file=txtfile)
+print("Greatest Increase in Profits:", str(Greatest_Inc_Date)[1:-1], '${:}'.format(Increase), file=txtfile)
+print("Greatest Decrease in Profits:", str(Greatest_Dec_Date)[1:-1], '${:}'.format(Decrease), file=txtfile)
 
 #close the txt file so that the file can be written
 txtfile.close()
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,18 +1,81 @@
+
+#import the module used to read your csv
 import pandas as pd
 
+#my csv file location - C:/Users/tmlun/python-challenge/PyPoll/Resources
 mycsv = "C:\\Users\\tmlun\\python-challenge\\PyPoll\\Resources\\election_data.csv"
 
-#C:/Users/tmlun/python-challenge/PyPoll/Resources
-
-
+#read csv and create DataFrame
 mydf = pd.read_csv(mycsv)
 
+#count the total # of votes in the dataset
 Total_Votes = mydf["Voter ID"].count()
-print(Total_Votes)
 
+#get the list of candidates that recieved votes
+Candi_Name = mydf["Candidate"].unique()
 
-cand_names = mydf["Candidate"].unique()
-print(cand_names)
+#find and count the # of votes per candidate and get percentage of votes
+    #set the names of the candidates
+names = mydf["Candidate"].values 
 
-votes_percent = cand_names.value_counts()
-print(votes_percent)
+    #lists to hold the # of votes for each candidate
+Khan_vote=[]
+Correy_vote=[]
+Li_vote=[]
+OTooley_vote=[]
+
+    #for loop to go through votes and add to the correct list for each candidate
+for i in names:
+    if i == "Khan":
+        Khan_vote.append(i)
+    elif i == "Correy":
+        Correy_vote.append(i)
+    elif i == "Li":
+        Li_vote.append(i)
+    elif i == "O'Tooley":
+        OTooley_vote.append(i)
+
+#get the total vote count for each candidaten  
+K_V = Khan_vote.count("Khan")
+C_V = Correy_vote.count("Correy")
+L_V = Li_vote.count("Li")
+O_V = OTooley_vote.count("O'Tooley")
+
+#get the vote percentage for each candidate
+Khan_Percent = round((K_V/Total_Votes),2)
+Correy_Percent = round((C_V/Total_Votes),2)
+Li_Percent = round((L_V/Total_Votes),2)
+OTooley_Percent = round((O_V/Total_Votes),2)
+
+#Print all the data to the terminal
+print("Election Results")
+print("-----------------------------------------")
+print("Total Votes:", Total_Votes)
+print("-----------------------------------------")
+print(Candi_Name[0],":",'{:.3%}'.format(Khan_Percent), K_V)
+print(Candi_Name[1],":",'{:.3%}'.format(Correy_Percent), C_V)
+print(Candi_Name[2],":",'{:.3%}'.format(Li_Percent), L_V)
+print(Candi_Name[3],":",'{:.3%}'.format(OTooley_Percent), O_V)
+print("-----------------------------------------")
+print("Winner:",Candi_Name[0])
+print("-----------------------------------------")
+
+# #define an output path for a text file
+output_path = "C:\\Users\\tmlun\\python-challenge\\PyPoll\\Analysis\\textfile.txt"
+txtfile = open(output_path,"w+")
+
+# #Print all the data to the txt file
+print("Election Results", file=txtfile)
+print("-----------------------------------------", file=txtfile)
+print("Total Votes:", Total_Votes, file=txtfile)
+print("-----------------------------------------", file=txtfile)
+print(Candi_Name[0],":",'{:.3%}'.format(Khan_Percent), K_V, file=txtfile)
+print(Candi_Name[1],":",'{:.3%}'.format(Correy_Percent), C_V, file=txtfile)
+print(Candi_Name[2],":",'{:.3%}'.format(Li_Percent), L_V, file=txtfile)
+print(Candi_Name[3],":",'{:.3%}'.format(OTooley_Percent), O_V, file=txtfile)
+print("-----------------------------------------", file=txtfile)
+print("Winner:",Candi_Name[0], file=txtfile)
+print("-----------------------------------------", file=txtfile)
+
+# #close the txt file so that the file can be written
+txtfile.close()
